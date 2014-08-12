2676d2363a8fff6f5c4f019486d62fdd710ec033
Matteo Alessani <alessani@gmail.com>
2014-08-12 09:40:31 +0200

    memcached

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,9 +12,9 @@ class memcached {
     description => "Memcached upstart",
     version => "1.0",
     respawn => true,
-    respawn_limit => "5 10",
+    respawn_limit => '5 10',
     user => 'memcache',
-    exec => '/usr/bin/memcached -v -m 1024 -p 11211 -u memcache -l 0.0.0.0 -c 1024 -I 1'
+    exec => '/usr/bin/memcached -v -m 1024 -p 11211 -u memcache -l 0.0.0.0 -c 1024 -I 1',
     require => Tidy['/etc/init.d/memcached'],
   }
 }
